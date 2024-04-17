9c5d3c371e337afaadb539aabe893446e91102f2
Fajri Tsani <fajritsani@Fajris-MacBook-Pro.local>
2024-04-17 07:18:23 +0700

    update sayhello with parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/fajritsaniy/go-say-hello
+module github.com/fajritsaniy/go-say-hello/v2
 
 go 1.22.2
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Say Hello"
+func SayHello(name string) string {
+	return "Hello " + name
 }
